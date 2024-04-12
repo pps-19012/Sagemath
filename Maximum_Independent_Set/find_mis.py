2665090bb5c2d6b781a194c813ed92245f969993
Pushpendra Pratap Singh <76405318+pps-19012@users.noreply.github.com>
2024-04-12 18:30:59 +0530

    Update find_mis.py

--- Maximum_Independent_Set/find_mis.py
+++ Maximum_Independent_Set/find_mis.py
@@ -1,5 +1,11 @@
+# helper function to compute powerset
+def powerset(s):
+    x = len(s)
+    masks = [1 << i for i in range(x)]
+    for i in range(1 << x):
+        yield [ss for mask, ss in zip(masks, s) if i & mask]
+        
 # g: graph, td: tree decomposition
-
 def mis(g, td):
     max_nei = 0
     for v in td.vertices():
