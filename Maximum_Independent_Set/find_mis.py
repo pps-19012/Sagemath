9b650ddf1ec94b3512721703b576b053f358b1cb
Pushpendra Pratap Singh <76405318+pps-19012@users.noreply.github.com>
2024-04-17 21:49:46 +0530

    Update find_mis.py

--- Maximum_Independent_Set/find_mis.py
+++ Maximum_Independent_Set/find_mis.py
@@ -1,5 +1,13 @@
-# helper function to compute powerset
 def powerset(s):
+    """
+    Computes Powerset of a given Sage Set
+
+    Args:
+        s ('sage.sets.set.Set_object_enumerated_with_category'): Set S containing the vertices that are present in a node of tree decomposition of a graph.
+
+    Yields:
+        <class 'generator'>: Powerset of the set S
+    """
     x = len(s)
     masks = [1 << i for i in range(x)]
     for i in range(1 << x):
@@ -7,12 +15,55 @@ def powerset(s):
         
 # g: graph, td: tree decomposition
 def mis(g, td):
+    """
+    Determines the Maximum Independent Set (MIS) for a graph using its tree decomposition
+    
+    Args:
+        g ('sage.graphs.graph.Graph'): Arbitrary Graph G
+        td ('sage.graphs.graph.Graph'): Tree Decomposition of Graph G
+        
+    Algorithm:    
+    This function uses Dynamic Programming approach to find the MIS utilizing the treewidth parameter k from its tree decomposition.
+    
+        Notations:
+        - X_i: a node of the tree decomposition
+        - D_i: the union of the sets X_j descending from X_i
+        - A(S, i): the size of the largest independent subset I of D_i such that:
+            - I ∩ X_i = S
+        - B(S, i, j): the size of the largest independent subset I of D_i such that:
+            - X_i and X_j are adjacent pair
+            - X_i is farther from the root of the tree decomposition than X_j
+            - I ∩ X_i ∩ X_j = S
+        
+        Note: I am referring the bags of the tree decomposition as nodes in the following explanations.
+        
+        Implementation:
+        - We assume one of the nodes as root node and accordingly find leaf nodes.
+        - Initialize table A and table B using the vertices present in leaf nodes.
+        - For each next step, we move closer to the root node starting from the leaf nodes.
+        - Compute the next state of table A using table B by the following:
+            - A(S, i) = |S| +  Σ (B(S ∩ X_j, j, i) - |S ∩ X_j|) over all neighbours j
+            - To simplify, for every subset in each node we are computing the maximum independent set possible including that subset.
+        - Compute the next state of table B using table A by the following:
+            - B(S, i, j) = maximum of A(S', i) where S' is a subset of X_i and S = S' ∩ X_j
+            - To simplify, for every subset shared between two neighbours we are maximum independent set possible including that subset.
+            
+    This approach is faster as maximum number of subsets at any node can be at most 2^k where k is the tree width of the graph.
+    Thus, the time taken to find MIS is proportional to a constant 2^k.
+
+    Returns:
+        tuple('set', 'int'): (Maximum Independet Set (MIS), Cardinality of MIS)
+    """
+    
+    # Root node can be chose at random but here it is the bag with maximum of neighbours in tree decomposition.
+    # This is seems to make calculations a little faster as tree depth is usually less by choosing such a root node.
     max_nei = 0
     for v in td.vertices():
         if len(td.neighbors(v)) > max_nei:
             max_nei = len(td.neighbors(v))
             root_node = v
-            
+
+    # find leaf nodes
     bags = {}
     q = [(root_node, 0)]
     visit = set()
@@ -31,6 +82,7 @@ def mis(g, td):
         if childPresent == False:
             leaf_nodes.append(node)
 
+    # map bags to numbers
     bag_number = {}
     bn = 0
     for k, ver in bags.items():
@@ -38,11 +90,12 @@ def mis(g, td):
             bag_number[v] = bn
             bn += 1
 
+    # map numbers to bags
     number_to_bag = {}
     for k, v in bag_number.items():
         number_to_bag[v] = k
 
-    # Initialize table A
+    # initialize table A
     a_c = len(td.vertices())
     A = {None:{}}
     
@@ -66,13 +119,14 @@ def mis(g, td):
                 if node in leaf_nodes or nei in leaf_nodes:
                     leaf_pairs.append((bag_number[nei], bag_number[node]))
 
+    # initialize table B
     B = {None:{}}
-    # Bottom of B
+
     for i, j in leaf_pairs:
-        # Fill None
+        # fill null
         B[None][(i,j)] = number_to_bag[i] - number_to_bag[j]
         
-        # Fill Leaf Pairs
+        # fill leaf pairs
         inter = number_to_bag[i] & number_to_bag[j]
         for v in inter:
             if v not in B:
@@ -80,18 +134,23 @@ def mis(g, td):
             if v in (number_to_bag[i] & number_to_bag[j]):
                 B[v][(i,j)] = {v}
 
+    # compute rest of states of table A and table B
     for i in range(len(bags_processed_A)-1, -1, -1):
         if bags_processed_A[i]:
             continue
             
-        # Compute A(S, i)
+        # compute A(S, i)
+        
         powerset_of_bag_node = powerset(number_to_bag[i])
+        # iterate over all the subsets of X_i
         for k in powerset_of_bag_node:
             tmp = set(k)
             for nei in td.neighbors(number_to_bag[i]):
                 j = bag_number[nei]
                 if j < i:
                     continue
+                
+                # intersection with neighbours X_j to get MIS for each subtree descending from X_i
                 common = set(k) & set(nei)
                 if len(common) == 0:
                     if (j, i) in B[None]:
@@ -112,6 +171,8 @@ def mis(g, td):
                 s = tuple(k)    
                 if s not in B:
                     continue
+                
+            # set state in table A
             if s not in A:
                 A[s] = {}
             A[s][i] = tmp
@@ -123,12 +184,13 @@ def mis(g, td):
                 A[temp] = {}
                 A[temp][i] = tmp
 
-        # Compute B(S, i, j)
+        # compute B(S, i, j)
         for nei in td.neighbors(number_to_bag[i]):
             j = bag_number[nei]
             if j > i:
                 continue
-
+            
+            # iterate over all subsets S' of X_i
             for l in powerset(number_to_bag[i]):
                 if len(l) == 0:
                     s_prime = None
@@ -136,6 +198,8 @@ def mis(g, td):
                     s_prime = l[0]
                 else:
                     s_prime = tuple(l)
+                
+                # intersection with neighbours X_j to get MIS among all neighbours descending from X_i
                 k = set(l) & set(number_to_bag[j])
                 if len(k) == 0:
                     s = None
@@ -145,7 +209,8 @@ def mis(g, td):
                     s = tuple(k)
                 if (s_prime not in A) or (i not in A[s_prime]):
                     continue
-                    
+                
+                # set state in table B
                 if s not in B:
                     B[s] = {}
                 if (i, j) not in B[s]:
@@ -153,9 +218,11 @@ def mis(g, td):
                 else:
                     if len(B[s][(i, j)]) <= len(A[s_prime][i]):
                         B[s][(i, j)] = A[s_prime][i]
-                        
-        bags_processed_A[i] = True
         
+        # bag X_i is processed, move to its neighbours in next iteration                
+        bags_processed_A[i] = True
+
+    # iterate over A(S, 0) to find the largest independent set for X_i(i = 0) or root node
     mis_set = {}
     for v in A.keys():
         if 0 in A[v] and len(A[v][0]) > len(mis_set):
